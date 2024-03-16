d7c1cd293cc8fb5c4c68260d3dc58b131d2e90e3
Shubham Pareek <spareek@usfca.edu>
2024-03-16 15:35:43 -0700

    init

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from flask import Flask, request, g
+import praw
+import time
 import logging as log
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def handleGet():
     return
 
-@app.route('/', methods=['POST'])
-def handlePost():
-    return
-
 if __name__ == '__main__':
-    log.info("Starting flask server")
-    app.run(port=5000, debug=True)  # Run the Flask app
+    reddit = praw.Reddit(client_id ='',
+                     client_secret ='',
+                     user_agent ='dg-stream')
+
+    print(reddit.read_only)
+
+    subreddit = reddit.subreddit('wallstreetbets')  # Change 'python' to the desired subreddit name
+    print(subreddit)
+    stream = subreddit.stream.submissions()
+
+    # Continuously process live posts
+    for post in stream:
+        try:
+            # print(post.title)  # Print post title
+            # print(post.author)  # Print post author
+            # print(post.created_utc)  # Print post creation time
+            print(post.selftext)  # Print post content (if it's a text post)
+            print('-------------------------------------------------------')
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            # Sleep for some time before retrying
+            time.sleep(60)  # Sleep for 60 seconds before retrying
+    
+    # log.info("Starting flask server")
+    # app.run(port=5000, debug=True)  # Run the Flask app
